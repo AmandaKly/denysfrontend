e8446aa2e5520a517e592b7f98a0b3ddc70bc7d9
AmandaKly <amandaklly0@gmail.com>
2023-12-14 09:07:45 -0300

    Alterando URL 2.1

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-//const { execSync } = require('child_process');
+const urlApi = "http://backend-service:3000/dados";
+// const hostUrl = window.location.href
+// const host = hostUrl.split("//")[1]
+// const ip = host.split(":")[0]
 
-// Comando para obter o IP interno do serviço Kubernetes
-//const getIPCommand = "kubectl get svc apiclusteriphomolog -n homolog -o=jsonpath='{.spec.clusterIP}'";
-
-// Função para executar o comando e retornar a saída como string
-//function getKubernetesServiceIP() {
-  //try {
-    // Executa o comando e obtém a saída como string
-    //const ip = execSync(getIPCommand, { encoding: 'utf-8' });
-    //return ip.trim(); // Remove espaços em branco extras
-  //} catch (error) {
-    //console.error('Erro ao obter o IP do serviço Kubernetes:', error);
-    //return null;
-  //}
-//}
-
-// Obtém o IP do serviço e usa na variável urlApi
-//const ipFromKubernetes = getKubernetesServiceIP();
-//const urlApi = `http://${ipFromKubernetes}:3000/dados`;
-const hostUrl = window.location.href
-const host = hostUrl.split("//")[1]
-const ip = host.split(":")[0]
-const urlApi = "http://apiclusteriphomolog:3000/dados"
 // const baseUrl = window.location.origin;
 // const urlApi = `http://` + ip + ":3000/dados"
 
